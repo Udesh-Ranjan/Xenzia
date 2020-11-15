d325bb7f5a2117efce17d62efebfefefa0aeccd3
Udesh-Ranjan <ranjanudesh404@gmail.com>
2020-11-15 12:02:22 +0530

    Node ka colouring feature hata diya

--- src/Snake.h
+++ src/Snake.h
@@ -2,12 +2,14 @@ class Node{
     public :
     double x,y,prevX,prevY;
     int dir,prevDir;
+    float red,green,blue;
     Node *next;
     Node();
     Node(double,double);
     Node(double,double,int);
     void drawNode();
     void setDirection(int);
+    void setColor(float red,float green,float blue);
 };
 
 class snake{
@@ -17,11 +19,13 @@ class snake{
     static const int LEFT=3;
     static const int RIGHT=4;
     static const int INVALID=-1;
+    int leftBond,rightBond,topBond,bottomBond;
     Node head;
-    snake();
-    snake(double,double);
-    snake(double,double,int);
+    snake(int top,int bottom,int left, int right);
+    snake(double,double,int top,int bottom,int left, int right);
+    snake(double,double,int,int top,int bottom,int left, int right);
     void drawSnake();
     void addNode();
     void setDirection(int dir);
+    bool collision();
 };
